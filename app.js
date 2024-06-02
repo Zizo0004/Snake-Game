d096d814958c8f2ea02f6a5bf97ed56738d92d0b
ziyad <ziyad.alnawfal1@gmail.com>
2024-06-02 03:41:13 +0100

    grid flows nicely

--- app.js
+++ app.js
@@ -1,30 +1,48 @@
- x = 0;
- y = 0;
+ x = 290; // intial position of head
+ y = 310;
 
  var newElement = 0
  currentInterval = null; // Track the currently active interval
-
 tracker = null
 obje = document.getElementById("a"); // head 
-
 var rect = null
 gameon = true
+
+innercontainer2 = document.getElementById("Innercontainer2");
+for(let q=0; q<=1319;q++){
+    section = document.createElement("div")
+    innercontainer2.appendChild(section)
+}
+allDivs = document.querySelectorAll("div")
+
+
+obje.style.left = "292px"
+obje.style.top  = "314px"
+
+
+allDivs[100].style.backgroundColor = "black";
+obje.style.width = `${allDivs[400].getBoundingClientRect().width -4}px`
+obje.style.height = `${allDivs[400].getBoundingClientRect().height -4}px`
 
 document.addEventListener("keydown", (e) => {
     clearInterval(currentInterval); // Clear any existing interval first
     if(gameon === true){
         switch (e.key) {
             case "ArrowUp":
-                if(tracker == "DOWN"){ currentInterval = setInterval(movementDown, 65);}
+                if(tracker == "DOWN"){ currentInterval = setInterval(movementDown, 65); }
                 else{
                     tracker = "UP"
                     currentInterval = setInterval(movementTop, 65);   
+                   
                 }
                 break;
             case "ArrowDown":
-                if(tracker == "UP"){currentInterval = setInterval(movementTop, 65);}
+                if(tracker == "UP"){currentInterval = setInterval(movementTop, 65);
+                   
+                }
                     else{tracker = "DOWN"
-                    currentInterval = setInterval(movementDown, 65);}
+                    currentInterval = setInterval(movementDown, 65);
+                }
                 break;
             case "ArrowRight":
                 if(tracker == "LEFT"){currentInterval = setInterval(movementLeft, 65);}
@@ -58,12 +76,25 @@ document.addEventListener("keydown", (e) => {
         }
     }
 });
- var apple = document.getElementById("img")
+ var apple = document.getElementById("food")
+ intialpositionX = Math.round(allDivs[100].getBoundingClientRect().left); // Initial position of apple
+ intialpositionY = Math.round(allDivs[100].getBoundingClientRect().top);
+
+ apple.style.left = `${intialpositionX}px`
+ apple.style.top = `${intialpositionY}px`
+
+
+
  var computedStyle = window.getComputedStyle(apple);
   Left  = parseInt(computedStyle.left.slice(0,-2))
   Top  = parseInt(computedStyle.top.slice(0,-2))
  
- 
+  appleleft = parseInt(window.getComputedStyle(apple).left.replace(/px/g,""))
+  appletop = parseInt(window.getComputedStyle(apple).top.replace(/px/g,""))
+
+  console.log(appleleft,appletop)
+
+
 
  positions = []
  blocks = []
@@ -71,17 +102,27 @@ document.addEventListener("keydown", (e) => {
  function movementTop() {
     headLeft = `${parseInt(window.getComputedStyle(obje).left.replace(/px/g,""))}px`
     headTop  = `${parseInt(window.getComputedStyle(obje).top.replace(/px/g,""))}px`
-    y = y - 12;
+
+
+    headLeftDetect = parseInt(window.getComputedStyle(obje).left.replace(/px/g,""))
+    headTopDetect  = parseInt(window.getComputedStyle(obje).top.replace(/px/g,""))
+    y = y - allDivs[400].getBoundingClientRect().height;
     obje.style.top=`${y}px`;
 
     Nextheadleft = parseInt(window.getComputedStyle(obje).left.replace(/px/g,""))
     Nextheadtop  = parseInt(window.getComputedStyle(obje).top.replace(/px/g,""))
 
-    if(90 <= Nextheadleft && Nextheadleft <= 120 && 90 <= Nextheadtop && Nextheadtop <= 120){apple.style.display = `none`}
-   // console.log(95 <= Nextheadleft && Nextheadleft <= 100 && 95 <= Nextheadtop && Nextheadtop <= 100)
+    console.log(`head x: ${Nextheadleft}, apple x: ${appleleft}, head y: ${Nextheadtop}, apple y: ${appletop}`);
+   console.log(`Difference x: ${Math.abs(Nextheadleft - appleleft)}, Difference y: ${Math.abs(Nextheadtop - appletop)}`);
 
+   console.log(y)
 
-
+    if ((Nextheadleft - 10) <= appleleft && (Nextheadleft + 10) >= appleleft && 
+        (Nextheadtop - 10) <= appletop && (Nextheadtop + 10) >= appletop) {
+        console.log("Collision detected");
+        alert("Collision detected");
+        apple.style.display = `none`;
+    }
     for(let x = positions.length - 1; x >= 0; x--){
         if(x == 0){
             positions[0][0] = headLeft
@@ -111,7 +152,7 @@ document.addEventListener("keydown", (e) => {
 function movementDown() {
     headLeft = `${parseInt(window.getComputedStyle(obje).left.replace(/px/g,""))}px`
     headTop  = `${parseInt(window.getComputedStyle(obje).top.replace(/px/g,""))}px`
-    y = y + 12;
+    y = y + allDivs[400].getBoundingClientRect().height;
     obje.style.top=`${y}px`
 
     Nextheadleft = parseInt(window.getComputedStyle(obje).left.replace(/px/g,""))
@@ -155,13 +196,12 @@ function movementRight() {
     headLeft = `${parseInt(window.getComputedStyle(obje).left.replace(/px/g,""))}px`
     headTop  = `${parseInt(window.getComputedStyle(obje).top.replace(/px/g,""))}px`
 
-    x = x + 19;
+    x = x + allDivs[400].getBoundingClientRect().width;
     obje.style.left=`${x}px`
 
     Nextheadleft = parseInt(window.getComputedStyle(obje).left.replace(/px/g,""))
     Nextheadtop  = parseInt(window.getComputedStyle(obje).top.replace(/px/g,""))
 
-    console.log(x)
 
     if(90 <= Nextheadleft && Nextheadleft <= 120 && 90 <= Nextheadtop && Nextheadtop <= 120){apple.style.display = `none`}
 
@@ -198,7 +238,7 @@ function movementLeft() {
    
    headLeft = `${parseInt(window.getComputedStyle(obje).left.replace(/px/g,""))}px`
    headTop  = `${parseInt(window.getComputedStyle(obje).top.replace(/px/g,""))}px`
-   x = x - 19;
+   x = x - allDivs[400].getBoundingClientRect().width;
    obje.style.left=`${x}px`
 
    Nextheadleft = parseInt(window.getComputedStyle(obje).left.replace(/px/g,""))
